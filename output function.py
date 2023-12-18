27647cd2556efb286be48b7558807ed2efcd3ff5
RabiaA-arif <151766471+RabiaA-arif@users.noreply.github.com>
2023-12-18 20:14:27 +0500

    Add files via upload

---
+++ output function.py
@@ -0,0 +1,26 @@
+first_name = "rabia"
+last_name = "arif"
+print(first_name)
+print(last_name)
+full_name = first_name + last_name
+print("my full name is:" ,full_name)
+l=len(first_name)
+print("length of first name is:",l)
+l2=len(last_name)
+print(l2)
+l3=len(full_name)
+print(l3)
+#x = input("firstname:")
+#y = input("lastrname:")
+
+
+x = int(input("enter num1:"))
+y = int(input("enter num2:"))
+c = x+y
+print(c)
+d = x-y
+print(d)
+f = x*y
+print(f)
+g = x/y
+print(g)
